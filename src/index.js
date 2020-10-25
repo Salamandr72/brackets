9b91bb272d7cb7b15c41cd4112007fca7dd93c1b
Salamandr72 <Salamandr72@mail.ru>
2020-10-25 21:55:48 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.join('').replace(/[\,]/g, '');
+    let indexBrackets = [];
+    for (let i = 0; i < str.length; i++) {
+        let iB = config.indexOf(str[i]);
+        if (iB % 2 === 0) {
+            if (config[iB] === config[iB + 1]) {
+                if (iB === indexBrackets[indexBrackets.length - 1]) {
+                    indexBrackets.pop();
+                } else { indexBrackets.push(iB); }
+            } else {
+                indexBrackets.push(iB + 1);
+            }
+        } else {
+            if (indexBrackets.pop() !== iB) return false;
+        }
+    }
+    return indexBrackets.length === 0;
 }
